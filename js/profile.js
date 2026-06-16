e7bba3696ed4b09f7c6c11b65a93ea3b1df9bfe0
shahadsj <siddek-05devops@dipti.com.bd>
2026-06-16 11:28:48 +0600

    Add escapeHtml

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,5 @@
+function escapeHtml(s){if(!s)return"";return s.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"&#039;").replace(/"/g,"&quot;");}
+
 // ==================== PROFILE ====================
 function showProfile() {
     if (!APP.currentUser) { showLoginPage(); return; }
